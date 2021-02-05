9f0d8fca769957f300a36c0cfdec8f2b772391eb
raliya1037 <51006541+raliya1037@users.noreply.github.com>
2021-02-05 20:21:13 +0800

    Updated frontend, ActionCable and DB

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,20 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("semantic-ui-sass")
+require("jquery")
+
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade');
+  });
+  scroll_bottom();
+})
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.find(3)
+Message.create(body: "what is your age", user: user)
+user = User.find(5)
+Message.create(body: "Hi all", user: user)
